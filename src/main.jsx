ec17e7ccc5dfdaa4dfe44f61d85c289dc181a911
Alina Alieks <alieksieienko.alina@gmail.com>
2024-05-13 19:13:42 +0300

    name

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import "../node_modules/modern-normalize";
 import App from "./components/App/App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
